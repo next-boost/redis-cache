1c67a943e6ea70bbd725cb736fd3f9f41986ee85
Rakuraku Jyo <jyo.rakuraku@gmail.com>
2021-12-07 14:21:43 +0900

    Catch all errors

--- src/index.ts
+++ src/index.ts
@@ -14,34 +14,57 @@ class Cache {
   tbd = 3600 // time before deletion
 
   constructor({ uri, ttl, tbd }: CacheOptions = {}) {
-    this.redis = new Redis(uri || 'redis://127.0.1:6379')
-    if (ttl) this.ttl = ttl
-    if (tbd) this.tbd = tbd
+    try {
+      this.redis = new Redis(uri || 'redis://127.0.1:6379')
+      if (ttl) this.ttl = ttl
+      if (tbd) this.tbd = tbd
+    } catch (e) {
+      console.error(e)
+      process.exit(1)
+    }
   }
 
   async set(key: string, value: Buffer, ttl?: number) {
     if (!ttl) ttl = this.ttl
 
-    const now = new Date().getTime() / 1000
-    await this.redis.set(`${key}:ttl`, now + ttl, 'EX', this.tbd)
-    await this.redis.setBuffer(key, value, 'EX', this.tbd)
+    try {
+      const now = new Date().getTime() / 1000
+      await this.redis.set(`${key}:ttl`, now + ttl, 'EX', this.tbd)
+      await this.redis.setBuffer(key, value, 'EX', this.tbd)
+    } catch (e) {
+      console.error(e)
+    }
   }
 
   async get(key: string, defaultValue?: Buffer): Promise<Buffer | undefined> {
-    const rv = await this.redis.getBuffer(key)
-    if (!rv) return defaultValue
-    return rv
+    try {
+      const rv = await this.redis.getBuffer(key)
+      if (!rv) return defaultValue
+      return rv
+    } catch (e) {
+      console.error(e)
+      return defaultValue
+    }
   }
 
   async has(key: string): Promise<CacheStatus> {
-    const now = new Date().getTime() / 1000
-    const rv = await this.redis.get(`${key}:ttl`)
-    return !rv ? 'miss' : Number(rv) > now ? 'hit' : 'stale'
+    try {
+      const now = new Date().getTime() / 1000
+      const rv = await this.redis.get(`${key}:ttl`)
+      return !rv ? 'miss' : Number(rv) > now ? 'hit' : 'stale'
+    } catch (e) {
+      console.error(e)
+      return 'miss'
+    }
   }
 
   async del(key: string) {
-    await this.redis.del(key)
-    await this.redis.del(`${key}:ttl`)
+    try {
+      await this.redis.del(key)
+      await this.redis.del(`${key}:ttl`)
+    } catch (e) {
+      console.error(e)
+    }
   }
 }
 
